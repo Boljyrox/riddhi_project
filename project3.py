47a1bf0dc947538a113a71af98afe912d1f99c5f
balaji <balajisk805@gmail.com>
2026-07-15 19:09:15 +0800

    free wing

--- project3.py
+++ project3.py
@@ -52,6 +52,12 @@ def print_menu(menu, header):
             print(f'{item:<35}', end='')
         print()
 
+def get_total():
+    total = 0
+    for sn, order in enumerate(shopping_cart):
+        total += order[1] * order[2]
+    return total
+
 def get_valid_SN(menu, msg):
     while True:
         try:
@@ -63,27 +69,26 @@ def get_valid_SN(menu, msg):
         except ValueError:
             print(f'Please select a valid dish.')
 
-def get_valid_qnty(menu, msg):
+def get_valid_qnty():
     while True:
         try:
             dish_qty = int(input(f'Enter quantity required:'))
-            if dish_qty in menu:
+            if dish_qty > 1:
                 return dish_qty
-            else:
-                print('Invalid quantity.')
+            elif dish_qty < 1:
+                print('Please enter a valid quantity')
         except ValueError:
             print(f'Please enter a valid quantity.')
 
 def add_to_cart(menu, msg):
     dish_SN = get_valid_SN(menu,msg)
-    dish_qty = get_valid_qnty(menu,msg)
+    dish_qty = get_valid_qnty()
     cart = menu[dish_SN].copy()
     cart.append(dish_qty)
     shopping_cart.append(cart)
 
 
 def del_from_cart():
-    print(shopping_cart)
     while True:
         cart_SN = int(input(f'Select the dish SN to be deleted: '))
         if cart_SN > len(shopping_cart) or cart_SN <= 0:
@@ -142,13 +147,31 @@ def take_order():
         if continue_order == 'n':
             break
 
+def free_add_on():
+    while True:
+        addon_option = input('Do you want a free Chicken Wing? (y/n): ')
+        if addon_option == 'y':
+            free_item = add_on[6].copy()
+            free_item[0] = "FREE " + free_item[0]
+            free_item[1] = 0.00
+            free_item.append(1)
+            shopping_cart.append(free_item)
+            print(f'Added item to shopping cart!')
+            break
+        elif addon_option == 'n':
+            print(f'Thank You!')
+            break
+        else:
+            print('Invalid option.')
+
 def final_receipt(group):
-    total = 0
-    for sn, order in enumerate(shopping_cart):
-        total += order[1] * order[2]
+    total = get_total()
     print('\n'*3)
     print_shopping_cart()
     discount(total,group)
+
+
+
 
 
 def main():
@@ -189,6 +212,8 @@ def main():
             print_shopping_cart()
 
         elif action == 'f':
+            if get_total() >= 10:
+                free_add_on()
             group = student_or_staff()
             final_receipt(group)
             print("\nThank you for ordering! Order finalised.")
